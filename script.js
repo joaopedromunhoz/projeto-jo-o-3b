0986b14f51b7583297cb3462d6608a6b12d45224
joaopedromunhoz <141830496+joaopedromunhoz@users.noreply.github.com>
2025-06-05 19:45:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Efeito de rolagem suave ao clicar nos links do menu
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute("href"));
+    section.scrollIntoView({ behavior: "smooth" });
+  });
+});
